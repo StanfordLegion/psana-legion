2c5826a8e1c5601fbb137a860d0f7b108fd1cd35
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-07-07 12:06:40 -0700

    More hacking on initial prototype.

--- psana_legion.py
+++ psana_legion.py
@@ -20,20 +20,36 @@ from __future__ import print_function
 import legion
 import psana
 
+ds = psana.DataSource("exp=xpptut15:run=54:idx")
+det = psana.Detector("cspad")
+run = ds.runs().next()
+
+@legion.task
+def fetch(time):
+    event = run.event(time) # Fetches the data
+    raw = det.raw(event)
+    # ispace = legion.Ispace(raw.shape)
+    # fspace = legion.Fspace({'raw': legion.u16, 'calib': legion.f32})
+    # region = legion.Region(ispace, fspace)
+    # region.raw.copy(raw)
+    region = None
+
+    metadata = {'time': time}
+    return metadata, region
+
+@legion.task(privileges=[legion.RW])
+def process(metadata, region):
+    # print(region.raw.sum())
+    pass
+
 @legion.task
 def analyze(nevent, time):
-    ds = psana.DataSource("exp=xpptut15:run=54:idx")
-    det = psana.Detector("cspad")
-    run = ds.runs().next()
+    metadata, region = fetch(time).get()
+    process(metadata, region)
 
-    event = run.event(time)
-    print(event.get(psana.EventId))
-
-    print('%x' % legion.c.legion_runtime_get_executing_processor(legion._my.ctx.runtime, legion._my.ctx.context).id)
-
-    det.raw(event) # fetches the data
-    det.calib(event) # calibrates the data
-    # TODO: try to overlap fetch and calibrate
+# @legion.task
+def predicate(nevent, time):
+    return True
 
 # Define the main Python task. This task is called from C++. See
 # top_level_task in psana_legion.cc.
@@ -45,7 +61,13 @@ def main_task():
     # print(det.raw(evt))
     run = ds.runs().next()
     times = run.times()
-    for nevent in legion.IndexLaunch([10]): # Just take the first 10 for now
-        analyze(nevent, times[nevent])
+    blocksize = 10
+    for start in xrange(1): #xrange(0, len(times), blocksize):
+        stop = min(start + blocksize, len(times))
+        # print('block %s %s' % (start, stop))
+        for nevent in xrange(start, stop):
+            if predicate(nevent, times[nevent]):
+                # print('  idx %s' % nevent)
+                analyze(nevent, times[nevent])
 
 # TODO 2: fetch small data and filter
