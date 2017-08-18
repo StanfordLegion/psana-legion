4807c8da15c765bef5e71e8941d55ddc089fbdef
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-08-18 15:35:56 -0700

    Update test harness to use RAX mode.

--- psana_legion/psana_legion.py
+++ psana_legion/psana_legion.py
@@ -17,52 +17,79 @@
 
 from __future__ import print_function
 
+import itertools
 import legion
 import numpy
 import psana
 
+run_number = 54
+
 # These are initialized on every core at the beginning of time.
-ds = psana.DataSource("exp=xpptut15:run=54:idx")
-det = psana.Detector("cspad")
-run = ds.runs().next()
+ds = psana.DataSource('exp=xpptut15:run=%s:rax' % run_number)
+det = psana.Detector('cspad', ds.env())
+calib_filename = None
+calib_offset = None
+
+class Location(object):
+    __slots__ = ['filenames', 'offsets',
+                 'calib_filename', 'calib_offset']
+    def __init__(self, event):
+        offset = event.get(psana.EventOffset)
+        self.filenames = offset.filenames()
+        self.offsets = offset.offsets()
+        self.calib_filename = offset.lastBeginCalibCycleFilename()
+        self.calib_offset = offset.lastBeginCalibCycleOffset()
+    def __repr__(self):
+        return 'Location(%s, %s)' % (self.offsets, self.filenames)
 
 @legion.task
-def fetch(time):
-    event = run.event(time) # Fetches the data
+def fetch(loc):
+    print('fetch', loc)
+    global calib_filename, calib_offset
+    if calib_filename != loc.calib_filename or calib_offset != loc.calib_offset:
+        ds.jump(loc.calib_filename, loc.calib_offset)
+        calib_filename = loc.calib_filename
+        calib_offset = loc.calib_offset
+    event = ds.jump(loc.filenames, loc.offsets) # Fetches the data
     raw = det.raw(event)
     region = legion.Region.create(
         raw.shape,
         {'raw': legion.int16, 'calib': legion.float32})
     numpy.copyto(region.raw, raw, casting='no')
 
-    metadata = {'time': time}
-    return region, metadata
+    return region
 
-@legion.task(privileges=[legion.RW], leaf=True)
-def process(region, metadata):
+@legion.task(privileges=[None, legion.RW], leaf=True)
+def process(loc, region):
+    print('process', loc)
     print(region.raw.sum())
 
 @legion.task(inner=True)
-def analyze(time):
-    region, metadata = fetch(time).get()
-    process(region, metadata)
+def analyze(loc):
+    region = fetch(loc).get()
+    process(loc, region)
     region.destroy()
 
 # This is so short it's not worth running as a task.
 # @legion.task
-def predicate(time):
+def predicate(event):
     return True
+
+def chunk(iterable, chunksize):
+    it = iter(iterable)
+    while True:
+        value = [next(it)]
+        value.extend(itertools.islice(it, chunksize-1))
+        yield value
 
 # Define the main Python task. This task is called from C++. See
 # top_level_task in psana_legion.cc.
 @legion.task(inner=True)
 def main_task():
-    times = run.times()
-    blocksize = 10
-    nevents = 10 # len(times)
-    for start in xrange(0, nevents, blocksize):
-        stop = min(start + blocksize, nevents)
-        for idx in xrange(start, stop):
-            # small = fetch_small(times[idx]) # TODO: Fetch small data.
-            if predicate(times[idx]):
-                analyze(times[idx])
+    ds2 = psana.DataSource('exp=xpptut15:run=%s:smd' % run_number)
+
+    chunksize = 10
+    for events in chunk(itertools.ifilter(predicate, ds2.events()), chunksize):
+        for event in events: # TODO: Index launch
+            analyze(Location(event))
+        break
