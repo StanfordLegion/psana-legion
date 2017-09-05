28d42750b43a601e92a56d74499d5b046a458fb8
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-05 10:22:28 -0700

    Fix typo.

--- psana_legion/scripts/sbatch_cori.sh
+++ psana_legion/scripts/sbatch_cori.sh
@@ -23,5 +23,5 @@ srun -n 1 --ntasks-per-node 1 --cpu_bind none \
   shifter --image=$IMAGE \
     --volume=$HOST_PSANA_DIR:/native-psana-legion \
     --volume=$HOST_LEGION_DIR:/legion \
-    --volume=$HOST_DATA_DIR:/reg
+    --volume=$HOST_DATA_DIR:/reg \
     /native-psana-legion/psana_legion/scripts/psana_legion.sh -ll:py 1 -ll:io 1
