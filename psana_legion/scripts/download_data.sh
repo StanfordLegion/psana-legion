dff9736b0245d467a74855782dfc72490e0e1474
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-05 13:23:47 -0700

    Fix download script.

--- psana_legion/scripts/download_data.sh
+++ psana_legion/scripts/download_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 machine="$1"@psexport.slac.stanford.edu
-dest_dir="$2"
+dest_dir="$2"/reg/d/psdm
 
-mkdir -p $dest_dir/psdm/xpp/xpptut15/xtc
-mkdir -p $dest_dir/psdm/xpp/xpptut15/xtc/index
-mkdir -p $dest_dir/psdm/xpp/xpptut15/xtc/smalldata
-rsync -rP $machine':/reg/d/psdm/xpp/xpptut15/xtc/*-r0054-*' $dest_dir/psdm/xpp/xpptut15/xtc
-rsync -rP $machine':/reg/d/psdm/xpp/xpptut15/xtc/index/*-r0054-*' $dest_dir/psdm/xpp/xpptut15/xtc/index
-rsync -rP $machine':/reg/d/psdm/xpp/xpptut15/xtc/smalldata/*-r0054-*' $dest_dir/psdm/xpp/xpptut15/xtc/smalldata
-rsync -rPL $machine':/reg/d/psdm/xpp/xpptut15/calib' $dest_dir/psdm/xpp/xpptut15/calib
-ln -s xpp $dest_dir/psdm/XPP
+mkdir -p $dest_dir/xpp/xpptut15/xtc
+mkdir -p $dest_dir/xpp/xpptut15/xtc/index
+mkdir -p $dest_dir/xpp/xpptut15/xtc/smalldata
+rsync -rP $machine':/reg/d/psdm/xpp/xpptut15/xtc/*-r0054-*' $dest_dir/xpp/xpptut15/xtc
+rsync -rP $machine':/reg/d/psdm/xpp/xpptut15/xtc/index/*-r0054-*' $dest_dir/xpp/xpptut15/xtc/index
+rsync -rP $machine':/reg/d/psdm/xpp/xpptut15/xtc/smalldata/*-r0054-*' $dest_dir/xpp/xpptut15/xtc/smalldata
+rsync -rPL $machine':/reg/d/psdm/xpp/xpptut15/calib' $dest_dir/xpp/xpptut15/calib
+ln -s xpp $dest_dir/XPP
