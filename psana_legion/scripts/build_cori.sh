94666d4c39624d8f4929ca9b95ea9f06ac9cb422
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-07 09:15:47 -0700

    Clean before build.

--- psana_legion/scripts/build_cori.sh
+++ psana_legion/scripts/build_cori.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-FORCE_PYTHON=1 PYTHON_LIB=/conda/lib/libpython2.7.so LG_RT_DIR=$HOME/psana_legion/legion/runtime DEBUG=0 make -j16
+export LG_RT_DIR=$HOME/psana_legion/legion/runtime
+
+make clean
+FORCE_PYTHON=1 PYTHON_LIB=/conda/lib/libpython2.7.so DEBUG=0 make -j16
