15624ce90a22887a0125eaf0e59a84f9bf64c0bf
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-08 13:12:16 -0700

    Add local directory to PYTHONPATH to make it easier to test changes.

--- psana_legion/scripts/psana_legion.sh
+++ psana_legion/scripts/psana_legion.sh
@@ -2,5 +2,6 @@
 
 root_dir="$(dirname "${BASH_SOURCE[0]}")"
 psana_dir="$(dirname "$root_dir")"
+export PYTHONPATH="$PYTHONPATH:$psana_dir"
 export LD_LIBRARY_PATH="$psana_dir:$psana_dir/lib64:$LD_LIBRARY_PATH"
 "/psana-legion/psana_legion/psana_legion" -ll:py 1 -ll:io 1 -ll:csize 16384 -lg:window 20
