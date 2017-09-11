d68eef61b3880b9951076a4add77057ed7057bab
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-11 16:06:30 -0700

    Basic parameter sweep with MPI.

--- psana_legion/scripts/sbatch_mpi_idx.sh
+++ psana_legion/scripts/sbatch_mpi_idx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=psana_legion
 #SBATCH --dependency=singleton
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --time=00:30:00
 #SBATCH --partition=debug # regular
 #SBATCH --constraint=knl,quad,cache
@@ -19,12 +19,22 @@ HOST_PSANA_DIR=$HOME/psana_legion/psana-legion
 HOST_DATA_DIR=$SCRATCH/data/reg
 # HOST_DATA_DIR=$SCRATCH/stripe_24_data/reg
 
-echo "Running MPI in IDX mode..."
-
 echo "HOST_DATA_DIR=$HOST_DATA_DIR"
 
-srun -n 16 -N 1 --cpus-per-task 4 --cpu_bind cores \
-  shifter --image=$IMAGE \
-    --volume=$HOST_PSANA_DIR:/native-psana-legion \
-    --volume=$HOST_DATA_DIR:/reg \
-    python /native-psana-legion/psana_legion/mpi_idx.py
+for n in 4 2 1; do
+  for c in 1 2 4 8 16 32 64; do
+    srun -n $(( n * c )) -N $n --cpus-per-task 4 --cpu_bind cores --output idx_n"$n"_c"$c".log \
+      shifter --image=$IMAGE \
+        --volume=$HOST_PSANA_DIR:/native-psana-legion \
+        --volume=$HOST_DATA_DIR:/reg \
+        python /native-psana-legion/psana_legion/mpi_idx.py
+  done
+
+  for c in 128 256; do
+    srun -n $(( n * c )) -N $n --cpus-per-task $(( 256/c )) --cpu_bind cores --output idx_n"$n"_c"$c".log \
+      shifter --image=$IMAGE \
+        --volume=$HOST_PSANA_DIR:/native-psana-legion \
+        --volume=$HOST_DATA_DIR:/reg \
+        python /native-psana-legion/psana_legion/mpi_idx.py
+  done
+done
