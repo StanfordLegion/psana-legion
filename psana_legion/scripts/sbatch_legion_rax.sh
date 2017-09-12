5679bd75d99dc02e9304834b32383ab89ac85ce8
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-11 22:59:33 -0700

    Initial sweep over number of Python processors.

--- psana_legion/scripts/sbatch_legion_rax.sh
+++ psana_legion/scripts/sbatch_legion_rax.sh
@@ -22,9 +22,18 @@ HOST_LEGION_DIR=$HOME/psana_legion/legion
 # Host directory where data is located
 HOST_DATA_DIR=$SCRATCH/data/reg
 
-srun -n 1 --ntasks-per-node 1 --cpu_bind none \
-  shifter --image=$IMAGE \
-    --volume=$HOST_PSANA_DIR:/native-psana-legion \
-    --volume=$HOST_LEGION_DIR:/legion \
-    --volume=$HOST_DATA_DIR:/reg \
-    /native-psana-legion/psana_legion/scripts/psana_legion.sh
+echo "HOST_DATA_DIR=$HOST_DATA_DIR"
+
+for n in 1; do
+  for c in 1 2 4 8 16 21; do
+  for i in 1; do
+    srun -n $(( n * c )) -N 1 --cpus-per-task $(( 256 / c )) --cpu_bind cores --output rax_n"$n"_c"$c"_i"$i".log \
+      shifter --image=$IMAGE \
+        --volume=$HOST_PSANA_DIR:/native-psana-legion \
+        --volume=$HOST_LEGION_DIR:/legion \
+        --volume=$HOST_DATA_DIR:/reg \
+        /native-psana-legion/psana_legion/scripts/psana_legion.sh \
+	  -ll:py 1 -ll:io $i -ll:csize 24576 -lg:window 20
+  done
+  done
+done
