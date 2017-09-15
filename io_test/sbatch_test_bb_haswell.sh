7c6ed293dcf0ffdcf98da429b2daba07f4f5d7c1
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-15 16:56:35 -0700

    Add scripts for testing burst buffer.

---
+++ io_test/sbatch_test_bb_haswell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=00:30:00
+#SBATCH --partition=debug # regular
+#SBATCH --constraint=haswell
+#SBATCH --mail-type=ALL
+#SBATCH --account=ACCOUNT
+#DW jobdw capacity=30GB access_mode=striped type=scratch
+#DW stage_in source=/global/cscratch1/sd/slaughte/data/reg/d/psdm/cxi/cxid9114/xtc/e438-r0108-s00-c00.xtc destination=$DW_JOB_STRIPED/e438-r0108-s00-c00.xtc type=file
+
+filename=$DW_JOB_STRIPED/e438-r0108-s00-c00.xtc
+
+for n in 1; do
+  for c in 1 2 4 8 16 32 64 128 256; do
+    for s in 1048576; do
+      srun -n $n -N $n --cpus-per-task $(( c * 2 )) --cpu_bind cores --output haswell_multi_n"$n"_c"$c"_s"$s".log \
+        ./io_test $filename $c $s
+
+      srun -n $n -N $n --cpus-per-task 2 --cpu_bind cores --output haswell_single_n"$n"_c"$c"_s"$s".log \
+        ./io_test $filename $c $s
+    done
+  done
+done
