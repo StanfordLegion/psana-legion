12403888d8361adc1e07b1b55b790c253183d05d
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-19 14:27:37 -0700

    Add script to allocate persistent BB.

---
+++ psana_legion/scripts/allocate_bb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=00:30:00
+#SBATCH --partition=debug
+#SBATCH --constraint=haswell
+#SBATCH --mail-type=ALL
+#SBATCH --account=ACCOUNT
+#BB create_persistent name=slaughte_data_noepics capacity=1TB access=striped type=scratch
