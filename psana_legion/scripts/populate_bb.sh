267a1250600f77256e38488e26d6197057b184ff
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-19 14:30:41 -0700

    Add script to populate BB.

---
+++ psana_legion/scripts/populate_bb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=00:30:00
+#SBATCH --partition=debug
+#SBATCH --constraint=haswell
+#SBATCH --mail-type=ALL
+#SBATCH --account=ACCOUNT
+#DW persistentdw name=slaughte_data_noepics
+
+export SRC=$SCRATCH/noepics_data/reg
+export DEST=$DW_PERSISTENT_STRIPED_slaughte_data_noepics
+export THREADS=32
+
+echo "Source $SRC"
+echo "Destination $DEST"
+
+echo "Create Directory"
+rsync -zr -f"+ */" -f"- *" $SRC/ $DEST/
+echo "Begin Transfer"
+cd $SRC && find . ! -type d -print0 | xargs -0 -n1 -P$THREADS -I% rsync -a % $DEST/%
+echo "Complete Transfer"
+du -sh $DEST
+ls -lR $DEST
