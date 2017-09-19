dde4856b0185d1f7f18f8bb7af70697605af37ac
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-19 14:37:30 -0700

    Fix populate script.

--- psana_legion/scripts/populate_bb.sh
+++ psana_legion/scripts/populate_bb.sh
@@ -8,13 +8,14 @@
 #DW persistentdw name=slaughte_data_noepics
 
 export SRC=$SCRATCH/noepics_data/reg
-export DEST=$DW_PERSISTENT_STRIPED_slaughte_data_noepics
+export DEST=$DW_PERSISTENT_STRIPED_slaughte_data_noepics/reg
 export THREADS=32
 
 echo "Source $SRC"
 echo "Destination $DEST"
 
 echo "Create Directory"
+mkdir $DEST
 rsync -zr -f"+ */" -f"- *" $SRC/ $DEST/
 echo "Begin Transfer"
 cd $SRC && find . ! -type d -print0 | xargs -0 -n1 -P$THREADS -I% rsync -a % $DEST/%
