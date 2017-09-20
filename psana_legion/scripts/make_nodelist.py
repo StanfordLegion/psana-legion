a0946c7a06b4b01295051b180ee35049abe4d871
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-20 10:09:30 -0700

    Add Python version of nodelist script.

---
+++ psana_legion/scripts/make_nodelist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import subprocess
+import sys
+
+assert len(sys.argv) == 2
+c = int(sys.argv[1]) # Number of ranks per node
+
+reserve = 1 # Number of ranks to be given dedicated nodes
+
+nodelist = subprocess.check_output(
+    ['scontrol', 'show', 'hostname', os.environ['SLURM_JOB_NODELIST']]).strip().split()
+first = nodelist[:reserve]
+rest = [x for node in nodelist[reserve:] for x in [node]*c]
+print('\n'.join(first+rest))
