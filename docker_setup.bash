b0c6018e41217eb3cb18d799e792cc3f3b359638
Alan Heirich <aheirich@sapling.stanford.edu>
2017-12-14 15:35:10 -0800

    correct gasnet path

--- docker_setup.bash
+++ docker_setup.bash
@@ -8,4 +8,4 @@ alias build_psana="FORCE_PYTHON=1 PYTHON_VERSION_MAJOR=2 PYTHON_LIB=/conda/lib/l
 export EXPERIMENT="exp=xpptut15:run=54:rax"
 export DETECTOR=cspad
 alias run_psana="EAGER=1 REALM_SYNTHETIC_CORE_MAP= mpirun -n 4 -genv USE_GASNET 1 ./psana_legion -ll:py 1 -ll:io 2 -ll:csize 6000 -lg:window 50 -level task_pool_mapper=1"
-export GASNET=/gasnet
+export GASNET=/gasnet/release/
