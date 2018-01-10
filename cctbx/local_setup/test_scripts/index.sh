d79c4811c0355c39dc16fd918b438b564017ca27
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-01-09 16:34:22 -0800

    Modify test scripts to call through Legion entrypoint.

--- cctbx/local_setup/test_scripts/index.sh
+++ cctbx/local_setup/test_scripts/index.sh
@@ -19,7 +19,32 @@ mkdir -p $OUT_DIR/discovery/dials/$RUN_F/$TRIAL_F/stdout
 mkdir -p $OUT_DIR/discovery/dials/$RUN_F/$TRIAL_F/tmp
 
 #run index
-cctbx.xfel.xtc_process \
+# The CCTBX wrapper script uses a Python wrapper that doesn't order PYTHONPATH
+# (et al) correctly, so we have to hack all the paths ourselves
+
+### Original version:
+#cctbx.xfel.xtc_process \
+
+# Here are the variables that were getting set in the command above.
+export PYTHONPATH="$CCTBX_PREFIX/modules/cctbx_project:$CCTBX_PREFIX/modules:$CCTBX_PREFIX/modules/cctbx_project/boost_adaptbx:$CCTBX_PREFIX/modules/cctbx_project/libtbx/pythonpath:$CCTBX_PREFIX/build/lib:$PYTHONPATH"
+export LD_LIBRARY_PATH="$CCTBX_PREFIX/build/lib:$PSANA_LEGION_DIR:$LD_LIBRARY_PATH"
+export PATH="$CCTBX_PREFIX/build/bin:$PATH"
+
+export LIBTBX_BUILD="$CCTBX_PREFIX/build"
+export LIBTBX_PYEXE_BASENAME="python2.7"
+export LIBTBX_DISPATCHER_NAME="cctbx.xfel.xtc_process"
+
+### This is what you would run, if we weren't using Legion:
+# $CONDA_PREFIX/bin/python -Qnew "$CCTBX_PREFIX/modules/cctbx_project/xfel/command_line/xtc_process.py" \
+
+### Legion version:
+export PSANA_MODULE=xtc_process
+export PSANA_MAPPER=simple
+export PYTHONPATH="$CCTBX_PREFIX/modules/cctbx_project/xfel/command_line:$PYTHONPATH"
+
+export PYTHONUNBUFFERED=1
+
+$PSANA_LEGION_DIR/psana_legion -ll:py 1 -ll:io 1 -ll:csize 6000 \
   input.experiment=$EXP \
   input.run_num=$RUN \
   output.logging_dir=$OUT_DIR/discovery/dials/$RUN_F/$TRIAL_F/stdout \
