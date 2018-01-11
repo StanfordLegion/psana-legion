566445089e17afe27eb8a7debb9b21f079fc3026
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-01-11 14:08:52 -0800

    Scripts for running CCTBX with Legion.

---
+++ cctbx/test_scripts/index_legion.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+START_XTC=$(date +"%s")
+EXP=$1
+RUN=$2
+TRIAL=$3
+
+# FIXME (Elliott): Does the trial number matter????
+
+source $CCTBX_PREFIX/build/setpaths.sh
+
+DATA_DIR=$SIT_PSDM_DATA/cxi/cxid9114/xtc
+
+# experiment parameters
+RUN_F="$(printf "r%04d" $RUN)"
+TRIAL_F="$(printf "%03d" $TRIAL)"
+
+# setup playground
+mkdir -p $OUT_DIR/discovery/dials/$RUN_F/$TRIAL_F/out
+mkdir -p $OUT_DIR/discovery/dials/$RUN_F/$TRIAL_F/stdout
+mkdir -p $OUT_DIR/discovery/dials/$RUN_F/$TRIAL_F/tmp
+
+#run index
+# The CCTBX wrapper script uses a Python wrapper that doesn't order PYTHONPATH
+# (et al) correctly, so we have to hack all the paths ourselves
+
+### Original version:
+#cctbx.xfel.xtc_process \
+
+# Here are the variables that were getting set in the command above.
+export PYTHONPATH="$CCTBX_PREFIX/modules/cctbx_project:$CCTBX_PREFIX/modules:$CCTBX_PREFIX/modules/cctbx_project/boost_adaptbx:$CCTBX_PREFIX/modules/cctbx_project/libtbx/pythonpath:$CCTBX_PREFIX/build/lib:$PYTHONPATH"
+export LD_LIBRARY_PATH="$CCTBX_PREFIX/build/lib:$PSANA_LEGION_DIR:$LD_LIBRARY_PATH"
+export PATH="$CCTBX_PREFIX/build/bin:$PATH"
+
+export LIBTBX_BUILD="$CCTBX_PREFIX/build"
+export LIBTBX_PYEXE_BASENAME="python2.7"
+export LIBTBX_DISPATCHER_NAME="cctbx.xfel.xtc_process"
+
+### This is what you would run, if we weren't using Legion:
+# $CONDA_PREFIX/bin/python -Qnew "$CCTBX_PREFIX/modules/cctbx_project/xfel/command_line/xtc_process.py" \
+
+### Legion version:
+export PSANA_MODULE=xtc_process
+export PSANA_MAPPER=simple
+export PYTHONPATH="$CCTBX_PREFIX/modules/cctbx_project/xfel/command_line:$PYTHONPATH"
+
+### Important: Put the EXTERNAL copy of psana_legion on path before the INTERNAL copy
+external_psana_dir=$PWD/../../psana-legion
+export PYTHONPATH="$external_psana_dir:$PYTHONPATH"
+export LD_LIBRARY_PATH="$external_psana_dir:$external_psana_dir/lib64:$LD_LIBRARY_PATH"
+
+$external_psana_dir/psana_legion -ll:py 1 -ll:io 1 -ll:csize 6000 \
+  input.experiment=$EXP \
+  input.run_num=$RUN \
+  output.logging_dir=$OUT_DIR/discovery/dials/$RUN_F/$TRIAL_F/stdout \
+  output.output_dir=$OUT_DIR/discovery/dials/$RUN_F/$TRIAL_F/out \
+  format.cbf.invalid_pixel_mask=$IN_DIR/mask_ld91.pickle \
+  $IN_DIR/process_batch.phil \
+  dump_indexed=False \
+  output.tmp_output_dir=$OUT_DIR/discovery/dials/$RUN_F/$TRIAL_F/tmp \
+  input.reference_geometry=$IN_DIR/geom_ld91.json \
+  input.xtc_dir=$DATA_DIR \
+  max_events=$LIMIT # Hack (FIXME: This doesn't actually do anything in Legion mode)
+
+END_XTC=$(date +"%s")
+ELAPSED=$((END_XTC-START_XTC))
+echo TotalElapsed_OneCore $ELAPSED $START_XTC $END_XTC
---
+++ cctbx/test_scripts/sbatch_legion_rax.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=00:30:00
+#SBATCH --partition=debug # regular
+#SBATCH --constraint=knl,quad,cache
+#SBATCH --core-spec=4
+#SBATCH --image=docker:stanfordlegion/cctbx-legion:latest
+#SBATCH --exclusive # causes shifter to preload image before run
+#SBATCH --mail-type=ALL
+#SBATCH --account=ACCOUNT
+
+# temporarily disable the following sbatch flags:
+# --job-name=psana_legion
+# --dependency=singleton
+
+# Host directory where data is located
+HOST_DATA_DIR=$SCRATCH/data/reg
+# HOST_DATA_DIR=$SCRATCH/stripe_24_data/reg
+# HOST_DATA_DIR=$SCRATCH/noepics_data/reg
+
+export SIT_PSDM_DATA=$HOST_DATA_DIR/d/psdm
+
+export IN_DIR=$PWD/input
+
+export EAGER=1
+export LIMIT=1
+
+for n in 8; do
+  echo "Running n$n"
+
+  export OUT_DIR=$PWD/output_legion_"$SLURM_JOB_ID"_n$n
+  mkdir $OUT_DIR
+
+  srun -n $n -N 1 --cpus-per-task $(( 256 / n )) --cpu_bind cores \
+    shifter ./index_legion.sh cxid9114 108 0 # 95 89 lustre
+done
