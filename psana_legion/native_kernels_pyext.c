af0ce7669453c8883eb076212f4a39e0059f96cd
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-02-06 14:09:26 -0800

    Add framework for native kernels.

---
+++ psana_legion/native_kernels_pyext.c
@@ -0,0 +1,43 @@
+/* Copyright 2018 Stanford University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "native_kernels.h"
+
+#include <Python.h>
+
+static PyObject *pyext_memory_bound_kernel(PyObject *self, PyObject *args)
+{
+  unsigned long long buffer_size;
+  unsigned long long rounds;
+
+  if (!PyArg_ParseTuple(args, "KK", &buffer_size, &rounds)) {
+    PyErr_SetString(PyExc_TypeError, "incorrect arguments to memory_bound_kernel");
+    return NULL;
+  }
+
+  memory_bound_kernel(buffer_size, rounds);
+  Py_RETURN_NONE;
+}
+
+static PyMethodDef PyextMethods[] = {
+  {"memory_bound_kernel", pyext_memory_bound_kernel, METH_VARARGS,
+   "Execute memory-bound kernel."},
+  {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC initnative_kernels(void)
+{
+  (void) Py_InitModule("native_kernels", PyextMethods);
+}
---
+++ psana_legion/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Stanford University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from distutils.core import setup, Extension
+
+module = Extension('native_kernels',
+                   sources=['native_kernels.c',
+                            'native_kernels_pyext.c'])
+
+setup(name='native_kernels',
+      version='1.0',
+      description='Sample native kernels for psana-legion.',
+      ext_modules=[module])
