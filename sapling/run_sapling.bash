dc53d7e211d2bbe5b4402e688cdb9a5bbe3b61b9
Alan Heirich <aheirich@sapling.stanford.edu>
2018-02-13 15:18:39 -0800

    sapling run script

--- sapling/run_sapling.bash
+++ sapling/run_sapling.bash
@@ -1,4 +1,6 @@
+OUTPUT_DIR=${PWD}/${PSANA_MAPPER}_mapper_output
+mkdir -p ${OUTPUT_DIR}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ( pushd ${DIR}/../psana_legion && \
-EAGER=1 REALM_SYNTHETIC_CORE_MAP= mpiexec -n 3 -x USE_GASNET=1 -x LD_LIBRARY_PATH=${LD_LIBRARY_PATH} ./psana_legion -ll:py 1 -ll:io 1 -ll:csize 6000 -lg:window 50 -level lifeline_mapper=1 || echo FAILURE $? && \
+EAGER=1 REALM_SYNTHETIC_CORE_MAP= mpiexec -n 4 -x USE_GASNET=1 -x LD_LIBRARY_PATH=${LD_LIBRARY_PATH} ./psana_legion -ll:py 1 -ll:io 1 -ll:csize 6000 -lg:window 50 -level lifeline_mapper=1,task_pool_mapper=1 -logfile ${OUTPUT_DIR}/${PSANA_MAPPER}_%.log || echo FAILURE $? && \
 popd )
