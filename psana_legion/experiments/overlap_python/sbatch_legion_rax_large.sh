9dd29249b7570ac48644e4c4f30d72d8c0baac40
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-02-20 22:12:23 -0800

    Update experiment scripts.

--- psana_legion/experiments/overlap_python/sbatch_legion_rax_large.sh
+++ psana_legion/experiments/overlap_python/sbatch_legion_rax_large.sh
@@ -47,7 +47,7 @@ for rounds in 20 40 80; do
           srun -n $(( n * c + 1 )) -N $(( n + 1 )) --cpus-per-task $(( 256 / c )) --cpu_bind cores --distribution=arbitrary --output rax_rounds"$rounds"_n"$n"_c"$c"_i"$i".log \
             shifter \
               $HOST_PSANA_DIR/psana_legion/scripts/psana_legion.sh \
-                -ll:cpu 0 -ll:py 1 -ll:io 1 -ll:concurrent_io $i -ll:csize $(( 48000 / c )) -ll:rsize 0 -ll:gsize 0 -lg:window 100
+                -ll:cpu 0 -ll:py 1 -ll:io 1 -ll:concurrent_io $i -ll:csize $(( 48000 / c )) -ll:rsize 0 -ll:gsize 0 -ll:ib_rsize 0 -lg:window 100
                 # -lg:prof $(( n * c + 1 )) -lg:prof_logfile prof_rounds"$rounds"_n"$n"_c"$c"_i"$i"_%.gz
         fi
       done
