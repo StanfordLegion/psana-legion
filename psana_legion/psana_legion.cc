06c878c2dc39375718dd66c85140d46971f50513
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-03-21 22:54:36 -0700

    Remove dead code.

--- psana_legion/psana_legion.cc
+++ psana_legion/psana_legion.cc
@@ -34,21 +34,6 @@ enum FieldIDs {
   X_FIELD_ID = 1,
 };
 
-VariantID preregister_python_task_variant(
-  const TaskVariantRegistrar &registrar,
-  const char *module_name,
-  const char *function_name,
-  const void *userdata = NULL,
-  size_t userlen = 0)
-{
-  CodeDescriptor code_desc(Realm::Type::from_cpp_type<Processor::TaskFuncPtr>());
-  code_desc.add_implementation(new Realm::PythonSourceImplementation(module_name, function_name));
-
-  return Runtime::preregister_task_variant(
-    registrar, code_desc, userdata, userlen,
-    registrar.task_variant_name);
-}
-
 int main(int argc, char **argv)
 {
   // do this before any threads are spawned
