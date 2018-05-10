94211fad40471f5e83e5fe8d32289e6d157bbdbe
Alan Heirich <aheirich@stanford.edu>
2018-05-10 10:19:54 -0700

    add comments to docker build script

--- psana_legion/scripts/docker_build_alan.bash
+++ psana_legion/scripts/docker_build_alan.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 #docker login && docker build -t stanfordlegion/psana-legion -f Dockerfile.psana-legion . && docker push stanfordlegion/psana-legion
+echo run this script with sudo from the psana-legion/docker directory
 docker login && docker build -t stanfordlegion/psana-legion:alan -f Dockerfile.psana-legion . && docker push stanfordlegion/psana-legion:alan
