c5d227ebe7c6853408851a345bb2c016a7793cdc
Alan Heirich <aheirich@cori04.nersc.gov>
2018-05-14 12:05:29 -0700

    add some useful instructions to the docker build script

--- psana_legion/scripts/docker_build_alan.bash
+++ psana_legion/scripts/docker_build_alan.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-#docker login && docker build -t stanfordlegion/psana-legion -f Dockerfile.psana-legion . && docker push stanfordlegion/psana-legion
 echo run this script with sudo from the psana-legion/docker directory
+echo after this run shifter_pull_alan.bash on cori to pull the docker image
 docker login && docker build -t stanfordlegion/psana-legion:alan -f Dockerfile.psana-legion . && docker push stanfordlegion/psana-legion:alan
