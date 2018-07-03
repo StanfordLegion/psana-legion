43b2f4481842d1fe396e1c9c217660b397bfd3b5
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-07-03 15:02:29 -0700

    Sample script for merging.

---
+++ cctbx/local_setup_mpi/test_scripts/merge.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Download model files needed to run
+if [[ ! -e 4ngz.pdb ]]; then
+    phenix.fetch_pdb 4ngz --mtz
+fi
+
+export trial=0
+export nproc=2
+
+export TARDATA=output/discovery/dials/r0*/000/out/int*.pickle.tar
+
+export effective_params="d_min=2.0 \
+targlob=$TARDATA \
+model=4ngz.pdb \
+backend=FS \
+scaling.report_ML=True \
+pixel_size=0.11 \
+nproc=$nproc \
+postrefinement.enable=True \
+scaling.mtz_file=4ngz.mtz \
+scaling.mtz_column_F=f(+) \
+min_corr=-1.0 \
+output.prefix=$trial"
+
+cxi.merge ${effective_params}
+cxi.xmerge ${effective_params}
