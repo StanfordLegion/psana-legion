0fca01f3f544ffb149c6266ae12ba8e952c6e8d8
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-07-10 11:15:24 -0700

    Tweak run script.

--- local_setup/run_local.bash
+++ local_setup/run_local.bash
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/env.sh
-( pushd ${DIR}/../psana_legion && \
-REALM_FREEZE_ON_ERROR=1 EAGER=1 REALM_SYNTHETIC_CORE_MAP= mpirun -n 3 ./psana_legion -ll:py 1 -ll:io 1 -ll:csize 6000 -lg:window 50 || echo FAILURE $? && \
+( pushd ${PSANA_LEGION_DIR} && \
+REALM_FREEZE_ON_ERROR=1 PSANA_MAPPER=simple EAGER=1 LIMIT=100 REALM_SYNTHETIC_CORE_MAP= mpirun -n 3 ./psana_legion -ll:py 1 -ll:io 1 -ll:csize 6000 -lg:window 50 || echo FAILURE $? && \
 popd )
