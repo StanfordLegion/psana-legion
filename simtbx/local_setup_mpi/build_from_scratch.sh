ecdd86cc72d8c0f3b7015fbfcca9a7f8a3eafdaa
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-10-09 10:25:36 -0700

    Initial work on SIMTBX setup.

--- simtbx/local_setup_mpi/build_from_scratch.sh
+++ simtbx/local_setup_mpi/build_from_scratch.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-if [[ -z $SIT_PSDM_DATA ]]; then
-    echo "Please set SIT_PSDM_DATA and run again"
+if [[ -z $LS49_BIG_DATA ]]; then
+    echo "Please set LS49_BIG_DATA and run again"
     exit 1
 fi
-if [[ ! -d $SIT_PSDM_DATA/cxi ]]; then
-    echo "SIT_PSDM_DATA does not contain a subdirectory named 'cxi', are you sure it's pointing to the right place?"
+if [[ ! -f $LS49_BIG_DATA/1m2a.pdb ]]; then
+    echo "LS49_BIG_DATA does not contain a file named '1m2a.pdb', are you sure it's pointing to the right place?"
     exit 1
 fi
-if [[ ! -e phenix-installer.tar.gz ]]; then
-    echo "Please download Phenix from http://phenix-online.org"
-    echo "And name the file phenix-installer.tar.gz"
-    exit 1
-fi
+# if [[ ! -e phenix-installer.tar.gz ]]; then
+#     echo "Please download Phenix from http://phenix-online.org"
+#     echo "And name the file phenix-installer.tar.gz"
+#     exit 1
+# fi
 
 # Setup environment.
 cat > env.sh <<EOF
@@ -30,9 +30,12 @@ export LD_LIBRARY_PATH=\$CONDA_PREFIX/lib:\$LD_LIBRARY_PATH
 export CCTBX_PREFIX=$PWD/cctbx
 
 # variables needed for run only
-export SIT_PSDM_DATA=$SIT_PSDM_DATA
+export LS49_BIG_DATA=$LS49_BIG_DATA
 
 # variables needed to run CCTBX
+if [[ -d \$CONDA_PREFIX ]]; then
+  conda activate myenv
+fi
 if [[ -e \$CCTBX_PREFIX/build/setpaths.sh ]]; then
   source \$CCTBX_PREFIX/build/setpaths.sh
 fi
@@ -40,42 +43,52 @@ EOF
 
 root_dir=$PWD
 
-source env.sh
-
 # Clean up any previous installs.
-rm -rf $CONDA_PREFIX
-rm -rf $CCTBX_PREFIX
-rm -rf $GCC_WRAPPER_DIR
+rm -rf conda
+rm -rf cctbx
+rm -rf gcc_wrapper
+
+source env.sh
 
 # Install Conda environment.
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 bash Miniconda2-latest-Linux-x86_64.sh -b -p $CONDA_PREFIX
 rm Miniconda2-latest-Linux-x86_64.sh
-conda install -y scons cython libtiff=4.0.6 icu=54 future wxpython pillow mock pytest jinja2 scikit-learn tabulate
-conda install -y --channel conda-forge "mpich>=3" mpi4py h5py pytables orderedset
-conda install -y --channel lcls-rhel7 psana-conda ndarray
+
+curl -O https://raw.githubusercontent.com/nksauter/LS49/master/tests/dials_env.txt
+conda create -y --name myenv --file dials_env.txt --channel cctbx --channel conda-forge --channel defaults --channel bioconda
+conda activate myenv
 python -m pip install procrunner
 
-# CCTBX can't handle the new ABIs in GCC >= 5. Since the CCTBX build
-# system doesn't recognize the CXX environment variable, we use a
-# wrapper to pass the necessary flags.
-mkdir $GCC_WRAPPER_DIR
-cat > $GCC_WRAPPER_DIR/g++ <<EOF
-#!/bin/bash
+# # CCTBX can't handle the new ABIs in GCC >= 5. Since the CCTBX build
+# # system doesn't recognize the CXX environment variable, we use a
+# # wrapper to pass the necessary flags.
+# mkdir $GCC_WRAPPER_DIR
+# cat > $GCC_WRAPPER_DIR/g++ <<EOF
+# #!/bin/bash
 
-$(which g++) -std=c++11 -fabi-version=2 -D_GLIBCXX_USE_CXX11_ABI=0 "\$@"
-EOF
-chmod +x $GCC_WRAPPER_DIR/g++
+# $(which g++) -std=c++11 -fabi-version=2 -D_GLIBCXX_USE_CXX11_ABI=0 "\$@"
+# EOF
+# chmod +x $GCC_WRAPPER_DIR/g++
 
 # Build CCTBX.
 mkdir $CCTBX_PREFIX
 pushd $CCTBX_PREFIX
   tar xfzv $root_dir/phenix-installer.tar.gz --strip 1 --wildcards '*/modules/labelit'
   curl -O https://raw.githubusercontent.com/cctbx/cctbx_project/master/libtbx/auto_build/bootstrap.py
-  mkdir -p modules/cxi_xdr_xes
   python bootstrap.py hot --builder=xfel
   python bootstrap.py update --builder=dials
-  python bootstrap.py build --builder=xfel --with-python=$CONDA_PREFIX/bin/python --nproc $(nproc --all)
+  pushd $CCTBX_PREFIX/modules
+    git clone https://github.com/nksauter/LS49.git
+    cp $LS49_BIG_DATA/nanoBraggCUDA.cu $CCTBX_PREFIX/modules/cctbx_project/simtbx/nanoBragg
+  popd
+  mkdir $CCTBX_PREFIX/build
+  pushd $CCTBX_PREFIX/build
+    python $CCTBX_PREFIX/modules/cctbx_project/libtbx/configure.py --enable_openmp_if_possible=True --enable_cuda LS49 prime iota
+    source $CCTBX_PREFIX/build/setpaths.sh
+    make -j$(nproc --all)
+  popd
+  mkdir $CCTBX_PREFIX/test
 popd
 
 echo
