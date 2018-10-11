50602535b6d82824ac41e1446e3897d8c44bc72b
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-10-11 15:41:44 -0700

    Update local build so that it works with Byfl.

--- simtbx/local_setup_mpi/build_from_scratch.sh
+++ simtbx/local_setup_mpi/build_from_scratch.sh
@@ -11,12 +11,29 @@ if [[ ! -f $LS49_BIG_DATA/1m2a.pdb ]]; then
     exit 1
 fi
 
+if [[ -z $USE_BYFL ]]; then
+    echo "Please set USE_BYFL to 0 or 1 and run again"
+    exit 1
+fi
+if [[ $USE_BYFL -eq 1 && -z $BYFL_PREFIX ]]; then
+    echo "Please set BYFL_PREFIX and run again"
+    exit 1
+fi
+
 # Setup environment.
 cat > env.sh <<EOF
+# variables needed for Byfl
+export USE_BYFL=$USE_BYFL
+export BYFL_PREFIX=$BYFL_PREFIX
+export BYFL_WRAPPER_DIR=$PWD/byfl_wrapper
+export BF_CLANG=clang-6.0
+export BF_CLANGXX=clang++-6.0
+export BF_OPTS="-bf-disable=byfl"
+
 # variables needed for conda
 export CONDA_PREFIX=$PWD/conda
 
-export PATH=\$CONDA_PREFIX/bin:\$PATH
+export PATH=\$CONDA_PREFIX/bin:\$BYFL_WRAPPER_DIR:\$PATH
 export LD_LIBRARY_PATH=\$CONDA_PREFIX/lib:\$LD_LIBRARY_PATH
 
 # variables needed for CCTBX
@@ -38,6 +55,7 @@ EOF
 root_dir=$PWD
 
 # Clean up any previous installs.
+rm -rf byfl_wrapper
 rm -rf conda
 rm -rf cctbx
 
@@ -54,6 +72,20 @@ conda create -y --name myenv --file dials_env.txt --channel cctbx --channel cond
 rm dials_env.txt
 conda activate myenv
 python -m pip install procrunner
+
+if [[ $USE_BYFL -eq 1 ]]; then
+  mkdir $BYFL_WRAPPER_DIR
+  cat >$BYFL_WRAPPER_DIR/gcc <<EOF
+$BYFL_PREFIX/bin/bf-clang "\$@"
+EOF
+  cat >$BYFL_WRAPPER_DIR/g++ <<EOF
+$BYFL_PREFIX/bin/bf-clang++ "\$@"
+EOF
+  chmod +x $BYFL_WRAPPER_DIR/gcc $BYFL_WRAPPER_DIR/g++
+
+  # ln -s $BYFL_PREFIX/bin/bf-clang $BYFL_WRAPPER_DIR/gcc
+  # ln -s $BYFL_PREFIX/bin/bf-clang++ $BYFL_WRAPPER_DIR/g++
+fi
 
 # Build CCTBX.
 mkdir $CCTBX_PREFIX
@@ -72,9 +104,24 @@ pushd $CCTBX_PREFIX
   cp $LS49_BIG_DATA/nanoBraggCUDA.cu $CCTBX_PREFIX/modules/cctbx_project/simtbx/nanoBragg
   mkdir $CCTBX_PREFIX/build
   pushd $CCTBX_PREFIX/build
-    python $CCTBX_PREFIX/modules/cctbx_project/libtbx/configure.py --enable_openmp_if_possible=True --enable_cuda LS49 prime iota
+    # Build first time with Byfl disabled
+    export BF_OPTS="-bf-disable=byfl"
+    if [[ $USE_BYFL -eq 1 ]]; then
+      python $CCTBX_PREFIX/modules/cctbx_project/libtbx/configure.py --enable_openmp_if_possible=False LS49 prime iota
+    else
+      python $CCTBX_PREFIX/modules/cctbx_project/libtbx/configure.py --enable_openmp_if_possible=True --enable_cuda LS49 prime iota
+    fi
     source $CCTBX_PREFIX/build/setpaths.sh
     make
+
+    # Now rebuild with Byfl enabled.
+    if [[ $USE_BYFL -eq 1 ]]; then
+      (
+        export BF_OPTS="-bf-by-func -bf-call-stack"
+        rm simtbx/nanoBragg/*.o
+        make
+      )
+    fi
   popd
 popd
 
