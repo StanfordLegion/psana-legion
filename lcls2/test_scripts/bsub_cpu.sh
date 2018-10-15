ac49908cb48aa81dd1b600192b82e02fafaa67f5
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-10-15 18:03:36 -0400

    Add proxy settings.

--- lcls2/test_scripts/bsub_cpu.sh
+++ lcls2/test_scripts/bsub_cpu.sh
@@ -15,4 +15,10 @@ export PS_PARALLEL=legion
 
 export LIMIT=10
 
+export all_proxy=socks://proxy.ccs.ornl.gov:3128/
+export ftp_proxy=ftp://proxy.ccs.ornl.gov:3128/
+export http_proxy=http://proxy.ccs.ornl.gov:3128/
+export https_proxy=https://proxy.ccs.ornl.gov:3128/
+export no_proxy='localhost,127.0.0.0/8,*.ccs.ornl.gov,*.ncrc.gov'
+
 jsrun -n 1 legion_python user -ll:py 1 -ll:cpu 1
