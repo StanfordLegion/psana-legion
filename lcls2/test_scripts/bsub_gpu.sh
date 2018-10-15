1810019a53c38d2fc75ece0ef3d81a2b091c6482
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-10-15 18:03:49 -0400

    Add GPU run script.

---
+++ lcls2/test_scripts/bsub_gpu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#BSUB -P CSC103SUMMITDEV
+#BSUB -W 0:30
+#BSUB -nnodes 1
+#BSUB -o lsf-%J.out
+#BSUB -e lsf-%J.err
+#BSUB -N
+
+root_dir="$PWD"
+export PYTHONPATH="$PYTHONPATH:$root_dir"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$root_dir/build"
+# uncomment this line when building Legion outside of conda build
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$REL_DIR/lib"
+export PS_PARALLEL=legion
+
+export LIMIT=10
+
+export all_proxy=socks://proxy.ccs.ornl.gov:3128/
+export ftp_proxy=ftp://proxy.ccs.ornl.gov:3128/
+export http_proxy=http://proxy.ccs.ornl.gov:3128/
+export https_proxy=https://proxy.ccs.ornl.gov:3128/
+export no_proxy='localhost,127.0.0.0/8,*.ccs.ornl.gov,*.ncrc.gov'
+
+jsrun -n 1 -g 1 legion_python user -ll:py 1 -ll:cpu 0 -ll:gpu 1
