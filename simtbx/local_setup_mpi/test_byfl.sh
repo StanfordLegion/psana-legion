a423f29be5ecca45b27d0871fd043b88c989ca25
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-10-15 20:23:39 -0700

    Update Byfl config.

--- simtbx/local_setup_mpi/test_byfl.sh
+++ simtbx/local_setup_mpi/test_byfl.sh
@@ -7,6 +7,12 @@ source env.sh
 rm -rf test_byfl
 mkdir test_byfl
 pushd test_byfl
-  export TEST_NUM_MOSAIC_DOMAINS=1
-  time libtbx.python $CCTBX_PREFIX/modules/LS49/tests/tst_monochromatic_image.py
+  for n in 1 2 5 10 25 50; do
+    mkdir test$n
+    pushd test$n
+      export TEST_NUM_MOSAIC_DOMAINS=$n
+      export BF_BINOUT="$PWD"/test.byfl
+      time ( libtbx.python $CCTBX_PREFIX/modules/LS49/tests/tst_monochromatic_image.py 2>&1 | tee out.log )
+    popd
+  done
 popd
