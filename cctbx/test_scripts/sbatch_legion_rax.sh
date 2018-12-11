d24bd24d42613af65a42f38507eea43c5195c871
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-12-11 15:02:27 -0800

    Update run script for CCTBX.

--- cctbx/test_scripts/sbatch_legion_rax.sh
+++ cctbx/test_scripts/sbatch_legion_rax.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#SBATCH --nodes=1
 #SBATCH --time=00:30:00
 #SBATCH --partition=debug # regular
 #SBATCH --constraint=knl,quad,cache
 #SBATCH --core-spec=4
-#SBATCH --image=docker:stanfordlegion/cctbx-legion:latest
+#SBATCH --image=docker:stanfordlegion/cctbx-legion:subprocess
 #SBATCH --exclusive # causes shifter to preload image before run
 #SBATCH --mail-type=ALL
 #SBATCH --account=m2859
@@ -45,9 +44,6 @@ done
 popd
 
 # Host directory where data is located
-# HOST_DATA_DIR=$SCRATCH/data/reg
-# HOST_DATA_DIR=$SCRATCH/stripe_24_data/reg
-# HOST_DATA_DIR=$SCRATCH/noepics_data/reg
 HOST_DATA_DIR=$SCRATCH/demo_data/reg
 
 export SIT_PSDM_DATA=$HOST_DATA_DIR/d/psdm
@@ -66,7 +62,7 @@ export GASNET_GNI_FMA_SHARING=1
 export MPICH_GNI_FMA_SHARING=enabled
 
 export REALM_BACKTRACE=1
-export GASNET_BACKTRACE=1
+# export GASNET_BACKTRACE=1
 
 # setting from Chris to avoid intermittent failures in PMI_Init_threads on large numbers of nodes
 export PMI_MMAP_SYNC_WAIT_TIME=600 # seconds
@@ -74,38 +70,47 @@ export PMI_MMAP_SYNC_WAIT_TIME=600 # seconds
 # fix to avoid crash on 1 and 2 cores/node
 export GASNET_USE_UDREG=0
 
+# enable new GASNet cutover mode
+export GASNET_GNI_AM_RVOUS_CUTOVER=1
+
+# try to fix memory probe issue in GASNetEx development snapshot
+export GASNET_MAX_SEGSIZE='1536M/P'
+
 set -x
 
 for n in $SLURM_JOB_NUM_NODES; do
-  for c in 4; do
-    export LIMIT=$(( 16 * n * c ))
+  for shard in 4; do
+    for py in 4; do
+      export LIMIT=$(( 16 * n * shard * py ))
 
-    export MAX_TASKS_IN_FLIGHT=$(( 1280 / c ))
+      export MAX_TASKS_IN_FLIGHT=$(( 1280 / shard / py ))
 
-    # export OUT_DIR=$PWD/output_legion_"$SLURM_JOB_ID"_n${n}_c${c}
-    export OUT_DIR=$SCRATCH/cori-cctbx/output_legion_"$SLURM_JOB_ID"_n${n}_c${c}
-    mkdir -p $OUT_DIR
-    mkdir -p $OUT_DIR/backtrace
+      # export OUT_DIR=$PWD/output_legion_"$SLURM_JOB_ID"_n${n}_shard${shard}
+      export OUT_DIR=$SCRATCH/cori-cctbx.subprocess/output_legion_"$SLURM_JOB_ID"_n${n}_shard${shard}
+      mkdir -p $OUT_DIR
+      mkdir -p $OUT_DIR/backtrace
 
-    echo "Running $(basename "$OUT_DIR")"
+      echo "Running $(basename "$OUT_DIR")"
 
-    # $HOST_PSANA_DIR/scripts/make_nodelist.py $c > $OUT_DIR/nodelist.txt
-    # export SLURM_HOSTFILE=$OUT_DIR/nodelist.txt
+      # $HOST_PSANA_DIR/scripts/make_nodelist.py $shard > $OUT_DIR/nodelist.txt
+      # export SLURM_HOSTFILE=$OUT_DIR/nodelist.txt
 
-    lmbsize=$(( 1024 * 32 * 32 / ( n * c ) )) # start shrinking at > 32 nodes * 32 ranks/node
-    if [[ $lmbsize -gt 1024 ]]; then
-        lmbsize=1024 # default is 1024 KB, don't go over default
-    fi
+      lmbsize=$(( 512 * 32 * 32 * 32 / ( n * shard * shard ) )) # start shrinking at >= 32 nodes * 32 ranks/node
+      if [[ $lmbsize -gt 1024 ]]; then
+          lmbsize=1024 # default is 1024 KB, don't go over default
+      fi
 
-    csize=$(( 48000 / c ))
-    if [[ $csize -gt 12000 ]]; then
-        csize=12000
-    fi
+      csize=$(( 16000 / shard ))
+      if [[ $csize -gt 8000 ]]; then
+          csize=8000
+      fi
 
-    # srun -n $(( n * c + 1 )) -N $(( n + 1 )) --cpus-per-task $(( 256 / c )) --cpu_bind cores --distribution=arbitrary \
-    srun -n $(( n * c )) -N $(( n )) --cpus-per-task $(( 256 / c )) --cpu_bind cores \
-      shifter /tmp/index_legion.sh cxid9114 108 0 \
-        -ll:cpu 0 -ll:py 1 -ll:io 1 -ll:concurrent_io 1 -ll:csize $csize -ll:rsize 0 -ll:gsize 0 -ll:ib_rsize 0 -ll:lmbsize $lmbsize -lg:window 100 -level announce=2 -logfile "$OUT_DIR/ann_%.log"
-    # -hl:prof $(( n * c )) -hl:prof_logfile "$OUT_DIR/prof_%.gz" 
+      # srun -n $(( n * shard + 1 )) -N $(( n + 1 )) --cpus-per-task $(( 256 / shard )) --cpu_bind cores --distribution=arbitrary \
+      srun -n $(( n * shard )) -N $(( n )) --cpus-per-task $(( 256 / shard )) --cpu_bind cores \
+        shifter /tmp/index_legion.sh cxid9114 108 0 \
+          -ll:cpu 0 -ll:py $py -ll:isolate_procs -ll:realm_heap_default -ll:io 1 -ll:concurrent_io 1 -ll:csize $csize -ll:rsize 0 -ll:gsize 0 -ll:ib_rsize 0 -ll:lmbsize $lmbsize -lg:window 100
+          # -level announce=2,activemsg=2,allocation=2 -logfile "$OUT_DIR/ann_%.log"
+          # -hl:prof $(( n * shard )) -hl:prof_logfile "$OUT_DIR/prof_%.gz"
+    done
   done
 done
