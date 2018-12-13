9e8d4e3aa78121f8236e4b5e03b787e2cc277272
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-12-13 10:01:31 -0800

    Fix typo.

--- cctbx/test_scripts/make_table.sh
+++ cctbx/test_scripts/make_table.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$SCRATCH/cori-cctbx.subprocess/output_*/discovery/dials/r0108/000/stdout/log_rank0000.out | sort -t_ -k5,5 -k7,7 -k9,9 -k11,11 -k3,3 -n | xargs grep '^Elapsed' | tr _ '\t' | tr / '\t' | sed 's/[.]/\t/2' | tr ' ' '\t' | cut -f 8,11,13,15,17,27
+ls $SCRATCH/cori-cctbx.subprocess/output_*/discovery/dials/r0108/000/stdout/log_rank0000.out | sort -t_ -k5,5 -k7,7 -k9,9 -k11,11 -k3,3 -n | xargs grep '^Elapsed' | tr _ '\t' | tr / '\t' | sed 's/[.]/\t/2' | tr ' ' '\t' | cut -f 8,11,13,15,17,27
