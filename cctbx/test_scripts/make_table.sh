f4741e332ef39b20edc5166519243c3a68e37bb4
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-12-18 13:15:47 -0800

    Sort output table properly.

--- cctbx/test_scripts/make_table.sh
+++ cctbx/test_scripts/make_table.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls $SCRATCH/cori-cctbx.subprocess/output_*/discovery/dials/r0108/000/stdout/log_rank0000.out | sort -t_ -k5,5 -k7,7 -k9,9 -k11,11 -k3,3 -n | xargs grep '^Elapsed' | tr _ '\t' | tr / '\t' | sed 's/[.]/\t/2' | tr ' ' '\t' | cut -f 8,11,13,15,17,27
+ls $SCRATCH/cori-cctbx.subprocess/output_*/discovery/dials/r0108/000/stdout/log_rank0000.out | sort -t_ -k2,2 -k5n,5n -k7n,7n -k9n,9n -k11n,11n -k3n,3n | xargs grep '^Elapsed' | tr _ '\t' | tr / '\t' | sed 's/[.]/\t/2' | tr ' ' '\t' | cut -f 8,11,13,15,17,27
