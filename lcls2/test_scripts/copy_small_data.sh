a4b0eabe4db226e0b89f4a2d6d9199fcbfae5a6a
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-03-27 19:35:51 -0400

    Copy all the data.

--- lcls2/test_scripts/copy_small_data.sh
+++ lcls2/test_scripts/copy_small_data.sh
@@ -4,4 +4,4 @@ dest=$MEMBERWORK/chm137/mona_small_data
 
 mkdir -p $dest
 
-scp $(whoami)@psexport.slac.stanford.edu:/reg/d/psdm/xpp/xpptut15/scratch/mona/xtc2/smalldata/*.xtc2 $dest
+rsync -rzP $(whoami)@psexport.slac.stanford.edu:/reg/d/psdm/xpp/xpptut15/scratch/mona/xtc2/ $dest
