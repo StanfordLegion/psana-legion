6431968f0e9bc3c5a37c7efa3547f254d25ed997
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-03-28 20:59:24 -0700

    Don't install by default, so that lcls2 can be rapidly developed.

--- lcls2/setup/dirty_rebuild.sh
+++ lcls2/setup/dirty_rebuild.sh
@@ -6,7 +6,7 @@ root_dir="$(dirname "${BASH_SOURCE[0]}")"
 source "$root_dir"/env.sh
 
 pushd $LCLS2_DIR
-./build_all.sh -d -p install
+./build_all.sh -d # -p install
 if [[ $(hostname --fqdn) != *"summit"* ]]; then
     pytest psana/psana/tests
 fi
