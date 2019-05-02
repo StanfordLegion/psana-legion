0aaf15b15e6f20da0c692c36ef00228c524b6182
Antoine Dujardin <dujardin@slac.stanford.edu>
2019-05-02 10:01:55 -0700

    Add phase retrieval code
    Add the code and the data. Import it. Don't run it yet.

---
+++ lcls2/proxy_app/phaseret/generator3D/README.md
@@ -0,0 +1,6 @@
+# Disclaimer
+
+The files in this folder come from:
+https://github.com/wang-zy/Tutorial-in-Diffraction-Imaging-Python
+
+They have been modified.
--- lcls2/proxy_app/user.py
+++ lcls2/proxy_app/user.py
@@ -26,7 +26,17 @@ import native_tasks
 import data_collector
 import solver
 
+import numpy as np
+import os.path
+from numpy import fft
+
+import phaseret
+from phaseret import InitialState, Phaser
+from phaseret.generator3D import Projection
+
+
 limit = int(os.environ['LIMIT']) if 'LIMIT' in os.environ else None
+
 
 xtc_dir = os.environ['DATA_DIR']
 ds = DataSource('exp=xpptut13:run=1:dir=%s'%(xtc_dir), max_events=limit, det_name='xppcspad')
@@ -46,5 +56,40 @@ for run in ds.runs():
     legion.execution_fence(block=True)
     data_collector.reset_data()
 
-# notes:
-# * what solves do we actually need
+
+# def create_dataset():
+#     cutoff = 2
+#     n_points = 64
+#     spacing = np.linspace(-cutoff, cutoff, 2*n_points+1)
+#     step = cutoff / n_points
+
+#     H, K, L = np.meshgrid(spacing, spacing, spacing)
+
+#     caffeine_pbd = os.path.join("caffeine.pdb")
+#     caffeine = Projection.Molecule(caffeine_pbd)
+
+#     caffeine_trans = Projection.moltrans(caffeine, H, K, L)
+#     caffeine_trans_ = fft.ifftshift(caffeine_trans)
+
+#     magnitude = np.absolute(caffeine_trans)
+
+#     return magnitude
+
+
+# def solve(magnitude):
+#     magnitude_ = fft.ifftshift(magnitude)
+#     initial_state = InitialState(magnitude_, is_ifftshifted=True)
+
+#     phaser = Phaser(initial_state)
+#     for k_cycle in range(2):
+#         phaser.HIO_loop(10, .1)
+#         phaser.ER_loop(10)
+#         phaser.shrink_wrap(.01)
+
+#     print("Fourier errors:")
+#     print(phaser.get_Fourier_errs()[0])
+#     print(phaser.get_Fourier_errs()[-1])
+
+#     print("Real errors:")
+#     print(phaser.get_real_errs()[0])
+#     print(phaser.get_real_errs()[-1])
