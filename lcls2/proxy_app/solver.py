530fd01554d377ea732e09170c89ba144f3e16ac
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-05-08 13:43:27 -0700

    Put an upper bound on the number of iterations.

--- lcls2/proxy_app/solver.py
+++ lcls2/proxy_app/solver.py
@@ -47,7 +47,7 @@ def solve():
 
     iteration = 0
     overall_answer = 0
-    while overall_answer == 0:
+    while overall_answer == 0 and iteration < 100:
         # Obtain the newest copy of the data.
         # FIXME: must epoch launch
         for idx in range(global_procs): # legion.IndexLaunch([global_procs]): # FIXME: index launch
