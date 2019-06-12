ae5f7a0b5f4fdc6fbf7501aff64b63147ce866c8
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-06-12 14:28:24 -0700

    Check in old state from experiments....

--- psana_legion/experiments/io_subprocess/make_mpi_table.sh
+++ psana_legion/experiments/io_subprocess/make_mpi_table.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls rax_*.log | sort -t_ -k3,3 -k5,5 -n | xargs grep '^Elapsed' | tr _ '\t' | sed 's/[.]/\t/1' | tr ' ' '\t' | cut -d$'\t' -f 3,5,8
+ls rax_*.log | sort -t_ -k3,3 -k5,5 -n | xargs grep '^Elapsed' | tr _ '\t' | sed 's/[.]/\t\t\t/1' | tr ' ' '\t' | cut -f 3,5,6,7,10
